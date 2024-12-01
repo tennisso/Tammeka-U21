f081a5319ff666550b6f8b5cf84900a603698934
54ndrq <sandratsirk@ut.ee>
2024-12-02 00:24:57 +0200

    stats lehe õige parandus (#5)

--- stats.js
+++ stats.js
@@ -1,37 +1,46 @@
 function sorteeriTabel(veerg) {
     const tabel = document.getElementById("statistikaTabel");
-    let vahetus, i, rida, järgmineRida, suund = "kasvav", vahetusi = true;
+    let i;
+    let rida;
+    let järgmineRida;
+    let suund = "kahanev";
+    let vahetusi = true;
 
     while (vahetusi) {
         vahetusi = false;
         const read = tabel.rows;
 
+        // Läbib kõik read (va. 1. rida)
         for (i = 1; i < read.length - 1; i++) {
-            vahetus = false;
+            let peaksVahetama = false;
             rida = read[i].cells[veerg].innerHTML.toLowerCase();
             järgmineRida = read[i + 1].cells[veerg].innerHTML.toLowerCase();
 
-            
             if (suund === "kasvav") {
                 if (isNaN(rida) || isNaN(järgmineRida)) {
-                    if (rida > järgmineRida) vahetus = true;
-                } else if (Number(rida) > Number(järgmineRida)) vahetus = true;
-            } else if (suund === "kahanev") {
+                    if (rida > järgmineRida) {
+                         peaksVahetama = true;
+                    }
+                } else if (Number(rida) > Number(järgmineRida)) { 
+                    peaksVahetama = true
+                };
+            }
+            else if (suund === "kahanev") {
                 if (isNaN(rida) || isNaN(järgmineRida)) {
-                    if (rida < järgmineRida) vahetus = true;
-                } else if (Number(rida) < Number(järgmineRida)) vahetus = true;
+                    if (rida < järgmineRida) { 
+                        peaksVahetama = true;
+                    }
+                } else if (Number(rida) < Number(järgmineRida)) {
+                    peaksVahetama = true;
+                }
             }
 
-            if (vahetus) {
+            // Kui vahetus on vajalik
+            if (peaksVahetama) {
                 read[i].parentNode.insertBefore(read[i + 1], read[i]);
                 vahetusi = true;
-                break;
+                break; // Välju välimisest tsüklist, kui vahetus on tehtud
             }
-        }
-        
-        if (!vahetusi && suund === "kasvav") {
-            suund = "kahanev";
-            vahetusi = true;
         }
     }
 }
